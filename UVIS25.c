83e6188fd90e452f931b88222460c35eab2819f5
chisl <info@chisl.io>
2018-01-02 16:25:31 +0100

    Initial commit

---
+++ UVIS25.c
@@ -0,0 +1,15 @@
+/*
+ * name:        UVIS25
+ * description: Digital UV index sensor: 0 - 15 UV index output range.
+ * manuf:       STMicroelectronics
+ * version:     0.1
+ * url:         http://www.st.com/resource/en/datasheet/uvis25.pdf
+ * date:        2018-01-02
+ */
+
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
+
+
